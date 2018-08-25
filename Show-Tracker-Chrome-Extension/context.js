124461491b5c056259a651dc64b03913fcf73304
Colin Rumball <colin.rumball@gmail.com>
2018-08-25 11:14:36 -0400

    Fixed error in chrome extension

--- Show-Tracker-Chrome-Extension/context.js
+++ Show-Tracker-Chrome-Extension/context.js
@@ -18,31 +18,26 @@ function SignIn(data, sender, sendResponse)
 
 function AddMagnetLink(data) 
 {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {data: "test"}, function(response) {
-            console.log('success');
+    let magnetLink = data.linkUrl;
+    fetch("http://shows.colinrumball.com/movie-torrents",{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+            link: magnetLink,
+        })
+    })
+    .then((response) =>
+    {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {data: "test"}, function(response) {
+                console.log('success');
+            });
         });
-    });
-    // let magnetLink = data.linkUrl;
-    // fetch("http://shows.colinrumball.com/movie-torrents",{
-    //     method: 'POST',
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    //     credentials: 'include',
-    //     body: JSON.stringify({
-    //         link: magnetLink,
-    //     })
-    // })
-    // .then((response) =>
-    // {
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //         chrome.tabs.sendMessage(tabs[0].id, {data: "test"}, function(response) {
-    //             console.log('success');
-    //         });
-    //     });
-    // })
-    // .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 }
 
 chrome.contextMenus.create({
